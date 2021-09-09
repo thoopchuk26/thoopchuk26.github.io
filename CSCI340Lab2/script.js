5d81a847e7623e7978bf929e80828cc6b30317f7
thoopchuk26 <thoopchuk@gmail.com>
2021-09-09 14:02:39 -0500

    fixing js typos

--- CSCI340Lab2/script.js
+++ CSCI340Lab2/script.js
@@ -13,14 +13,22 @@ $(document).ready(function() {
           success: function(results) {
             abilityName = results["spells"];
             $('#ability').attr("src", abilityName);
+            var noSpace = abilityName.replace(/\s/g, '+');
+            $.ajax({
+              dataType: "jsonp",
+              jsonpCallback: "parseQuote",
+              url: `https://api.spotify.com/v1/search?q=track"'${noSpace}'"/images`,
+              success: function(results){
+                $('abilityPic').attr('src', results);
+              },
+            })
           },
         })
       },
       error: function(xhr,status,error) {
         console.log(error);
-      }
+      },
     })
-  })
 
   $('.songTitle').click(function(){
     $.ajax({
@@ -29,34 +37,19 @@ $(document).ready(function() {
       url: `https://api.spotify.com/v1/search/${abilityName}`,
       success: function(results){
         $('#song').attr("src", results["Name"]);
+        var noSpace = abilityName.replace(/\s/g, '');
+        $.ajax({
+          dataType: "jsonp",
+          jsonpCallback: "parseQuote",
+          url: `http://ddragon.leagueoflegends.com/cdn/11.17.1/img/spell/${noSpace}.png`,
+          success: function(results){
+            $('abilityPic').attr('src', results);
+          }
+        })
       },
       error: function(xhr,status,error){
         console.log(error);
-      })
-    })
-  })
-
-  $('.abilityPicture').ready(function(){
-    var noSpace = abilityName.replace(/\s/g, '');
-    $.ajax({
-      dataType: "jsonp",
-      jsonpCallback: "parseQuote",
-      url: `http://ddragon.leagueoflegends.com/cdn/11.17.1/img/spell/${noSpace}.png`
-      success: function(results){
-        $('abilityPic').attr('src', results);
-      }
-    })
-  })
-
-  $('.songPicture').ready(function(){
-    var noSpace = abilityName.replace(/\s/g, '+');
-    $.ajax({
-      dataType: "jsonp",
-      jsonpCallback: "parseQuote",
-      url: `https://api.spotify.com/v1/search?q=track"'${noSpace}'"/images`
-      success: function(results){
-        $('abilityPic').attr('src', results);
-      }
+      },
     })
   })
 })
